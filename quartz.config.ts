5ceb97279aa2a1c3b99ff0bd6636d7d4767bbe2e
uriahby <uriah.poland@gmail.com>
2024-11-23 22:30:44 +0100

    Quartz sync: Nov 23, 2024, 10:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "JetBrains Mono",
+        header: "Alumni Sans Pinstripe",
+        body: "Alumni Sans Pinstripe",
         code: "IBM Plex Mono",
       },
       colors: {
