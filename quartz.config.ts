c07d486e0dbb8dea6bc810abe278499259cd0cd1
uriahby <uriah.poland@gmail.com>
2024-11-26 21:43:32 +0100

    Quartz sync: Nov 26, 2024, 9:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Данжн мяустер",
+    pageTitle: "Jürekmeister",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
